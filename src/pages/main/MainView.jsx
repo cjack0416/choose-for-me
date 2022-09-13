59400dd8ba649b209467610893dc9441825d12a3
Chris Jackson <chrisjackson0416@gmail.com>
2022-09-12 22:19:21 -0700

    Initial home page view

---
+++ src/pages/main/MainView.jsx
@@ -0,0 +1,15 @@
+import './index.css'
+
+const MainView = () => {
+    return(
+        <div>
+            <h1>Choose For Me</h1>
+            <div className='page-descriptions'>
+                <h2>set the options that you can’t decide from and we’ll pick for you </h2>
+                <h2>choose a category and location and we’ll find options for you and decide for you</h2>
+            </div>
+        </div>
+    );
+}
+
+export default MainView
