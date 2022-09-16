fd636933aa79d8e96f3e747b51e886b96b26ce36
Chris Jackson <chrisjackson0416@gmail.com>
2022-09-15 23:40:32 -0700

    Main page layout and responsiveness done

--- src/pages/main/MainView.jsx
+++ src/pages/main/MainView.jsx
@@ -1,13 +1,19 @@
 import './index.css'
+import { ReactComponent as BgShape} from '../../assets/backgroundWaveShape.svg'
 
 const MainView = () => {
     return(
         <div>
-            <h1>Choose For Me</h1>
-            <div className='page-descriptions'>
-                <h2>set the options that you can’t decide from and we’ll pick for you </h2>
-                <h2>choose a category and location and we’ll find options for you and decide for you</h2>
-            </div>
+            <BgShape className='bg-shape'/>
+            <div className='main-container'>
+                <h1>Choose For Me</h1>
+                <div className='pg-routes-grid'>
+                    <h2 className='pg-grid-item'>set the options that you can’t decide from and we’ll pick for you </h2>
+                    <btn className='pg-grid-item'>test</btn>
+                    <h2 className='pg-grid-item'>choose a category and location and we’ll find options for you and decide for you</h2>
+                    <btn className='pg-grid-item'>test</btn>
+                </div>
+            </div>  
         </div>
     );
 }
